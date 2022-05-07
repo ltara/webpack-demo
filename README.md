894f86eecf5d91d383951bedb0116701047fc1d1
caidengfeng <caidengfeng@qianxin.com>
2022-05-07 16:55:40 +0800

    feat: 添加解析文章地址

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # webpack-demo
+
+[Webapck5核心打包原理全流程解析](https://juejin.cn/post/7031546400034947108#heading-23)
--- test.js
+++
@@ -1,7 +0,0 @@
-const path = require('path')
-
-const fromPath = 'd:/Project/webpack'
-
-const toPath = 'd:/Project/webpack/example/src/module.js'
-
-console.log(path.posix.relative(fromPath, toPath))
